3edebb48fbf276938270b4f6cd1588a42d373abc
Toye Jeremiah <toyejeremiah@gmail.com>
2024-02-05 21:12:25 +0100

    another  commit

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token: 
-  "skKUBhBnJgJid42tBeXc7oOlMgQ5SYdL2MHw5B4dcy8PdfKyDOw7BlX9MDHfq4bcdLmsOXPQtEh73ZaEtjcrLNG3yAg7cmoyO4wCaASBLScqeYT4kUHdIHCcqs4WjjvBbfq5Oi43EYbplkzyyc83R4fXDZs8lExUUAqRpFSjJuSMQcG0eeeP"
 })
